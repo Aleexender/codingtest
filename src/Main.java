2e23b858807b2541650c422f5c5da457bebc0826
Alex Kim <alexkjh2011@gmail.com>
2022-11-15 01:34:01 +0900

    use a while & if & for in Main Class

--- src/Main.java
+++ src/Main.java
@@ -6,3 +6,28 @@ import java.util.Stack;
     public void test() {
         Scanner scanner = new Scanner(System.in);
         String a = "";
+
+        while (true) {
+
+            a = String.valueOf(scanner.nextInt());
+            if (a.equals("0")) {
+                break;
+            }
+            char[] num = new char[a.length()];
+            Stack<Character> stack = new Stack<>();
+
+            // 스택에 넣기
+            for (int i = 0; i < a.length(); i++) {
+                stack.add(a.charAt(i));
+            }
+            //스택에서 뺴기
+            for (int k = 0; k < a.length(); k++) {
+                num[k] = stack.pop();
+            }
+            //비교하기
+            if (a.equals(String.valueOf(num))) {
+                System.out.println("yes");
+            } else System.out.println("no");
+            ;
+        }
+
