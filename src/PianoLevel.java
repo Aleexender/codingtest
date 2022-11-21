7adec3c619819bb54f06cb3cf4e0b7224f9ecdb2
Alex Kim <alexkjh2011@gmail.com>
2022-11-22 02:57:18 +0900

    make a boolean first true & put for() to repeat it and compare put if() compare with index

--- src/PianoLevel.java
+++ src/PianoLevel.java
@@ -7,3 +7,12 @@ public class PianoLevel {
         for (int k = 0; k < a.length; k++) { // scanner에 대입하기
             a[k] = scanner.nextInt();
         }
+
+       // 오름차순
+        boolean ascending = true;
+        for (int i = 0; i < a.length -1; i++) {
+            if (a[i] > a[i + 1]) {
+                ascending = false;
+                break;
+            }
+        }
