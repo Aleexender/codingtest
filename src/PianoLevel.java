574cf1bbd59efb2e3f54481c86ebefb004b6173d
Alex Kim <alexkjh2011@gmail.com>
2022-11-22 02:56:31 +0900

    make a level method and make a Scanner to put number

--- src/PianoLevel.java
+++ src/PianoLevel.java
@@ -1,3 +1,9 @@
 import java.util.Scanner;
 
 public class PianoLevel {
+    public void level() {
+        int[] a = new int[8];
+        Scanner scanner = new Scanner(System.in);
+        for (int k = 0; k < a.length; k++) { // scanner에 대입하기
+            a[k] = scanner.nextInt();
+        }
