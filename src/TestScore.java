cd8ac635579fee965d6ec4ba03654da908e432dd
Alex Kim <alexkjh2011@gmail.com>
2022-12-04 03:06:30 +0900

    put if() and else if to evaluate socres

--- src/TestScore.java
+++ src/TestScore.java
@@ -3,3 +3,17 @@ public class TestScore {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int score = scanner.nextInt();
+
+        if (90 <= score) {
+            System.out.println("A");
+        } else if (80 <= score ) {
+            System.out.println("B");
+        } else if (70 <= score) {
+            System.out.println("C");
+        } else if (60 <= score) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
