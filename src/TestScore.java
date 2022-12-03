88ce71e7f7e88db3cb19c1a475fb9d72593237b9
Alex Kim <alexkjh2011@gmail.com>
2022-12-04 03:05:01 +0900

    put Scanner instance and include with int score

--- src/TestScore.java
+++ src/TestScore.java
@@ -1,3 +1,5 @@
 import java.util.Scanner;
 public class TestScore {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
