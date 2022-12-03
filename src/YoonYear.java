ba54327861d087e62ba8a274193b414c1eb8d274
Alex Kim <alexkjh2011@gmail.com>
2022-12-04 03:08:25 +0900

    make the final instance fields

--- src/YoonYear.java
+++ src/YoonYear.java
@@ -4,3 +4,6 @@ public class YoonYear {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
+
+        final int yoon =1;
+        final int nonYoon =0;
