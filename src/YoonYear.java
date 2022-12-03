5464a5a9b5cf49af220a9387180dfa2d8a8e7297
Alex Kim <alexkjh2011@gmail.com>
2022-12-04 03:07:58 +0900

    put a Scanner and include int year in Scanner to print from viewer

--- src/YoonYear.java
+++ src/YoonYear.java
@@ -2,3 +2,5 @@ import java.util.Scanner;
 
 public class YoonYear {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
