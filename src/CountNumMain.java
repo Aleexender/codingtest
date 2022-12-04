034b4d4a843522e1842c3d814c78a1e1e582794c
Alex Kim <alexkjh2011@gmail.com>
2022-12-05 00:07:39 +0900

    make the instance and call the instance of method

--- src/CountNumMain.java
+++ src/CountNumMain.java
@@ -1,3 +1,10 @@
 import java.io.IOException;
+
 public class CountNumMain {
     public static void main(String[] args) throws IOException {
+
+        CountNum countNum = new CountNum();
+        countNum.solution();
+
+    }
+}
