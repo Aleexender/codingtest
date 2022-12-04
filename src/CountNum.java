00858d3c18470b37819f399503a345074c3c8da9
Alex Kim <alexkjh2011@gmail.com>
2022-12-04 23:49:13 +0900

    put a for and if

--- src/CountNum.java
+++ src/CountNum.java
@@ -17,3 +17,10 @@ public class CountNum {
 
         String eachnum = calculate.toString();
 //        String eachnum2 = String.valueOf(cal);
+
+        for (int k = 0; k <= unexchange; k++ ) { // 1~9까지 돈다
+            for (int i = 0; i < eachnum.length(); i++) { //eachnum에 들어간 숫자를 한번씩 돈다
+                if(k == Character.getNumericValue(eachnum.charAt(i))){ // Character를 인트로 바꾼다 뭐를?  (String -> char 로 바꾼다.) --> 결론은 그냥 인트로 바꾸는거임 - k가 인트이기 때문에 인트로 비교를 해야함.
+                    count ++;
+                }
+            }
