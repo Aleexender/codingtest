9b3c8035de64d6028618424e96aa49bbc64da524
Alex Kim <alexkjh2011@gmail.com>
2022-12-06 03:29:18 +0900

    put instance and change char to int in AskiCode class

--- src/AskiCode.java
+++ src/AskiCode.java
@@ -3,3 +3,9 @@ import java.util.Scanner;
 public class AskiCode {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        char alpah = scanner.next().charAt(0);
+        int num = (int) alpah; // 강제적으로 char -> int 로 변환
+        System.out.println(num);
+
+    }
+}
