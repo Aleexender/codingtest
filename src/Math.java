057978da20ee6281c3a845aabeae606ac407cde4
Alex Kim <alexkjh2011@gmail.com>
2022-12-07 02:18:08 +0900

    make a Scanner

--- src/Math.java
+++ src/Math.java
@@ -2,3 +2,5 @@ import java.util.Scanner;
 
 public class Math {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
