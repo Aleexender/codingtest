5361a03dbe4460b3e2279b1c313b134bf1f9ce5c
Alex Kim <alexkjh2011@gmail.com>
2022-12-07 02:17:51 +0900

    make Math main method

--- src/Math.java
+++ src/Math.java
@@ -1,2 +1,4 @@
 import java.util.Scanner;
 
+public class Math {
+    public static void main(String[] args) {
--- src/Multiply.java
+++ src/Multiply.java
@@ -2,3 +2,16 @@ import java.util.Scanner;
 
 public class Multiply {
 
+    public void solution() {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        final int maximum = 9;
+
+        for (int i = 1; i <= maximum; i++) {
+            int answer = num * i;
+            System.out.println(num + " * " + i + " = "+ answer);
+        }
+
+
+    }
+}
