6fa86e5d384ff5e9c80456ede0294f2763985efb
Alex Kim <alexkjh2011@gmail.com>
2022-12-07 02:05:37 +0900

    make Sacnner in PutN class

--- src/PutN.java
+++ src/PutN.java
@@ -2,3 +2,5 @@ import java.util.Scanner;
 
 public class PutN {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num =  scanner.nextInt();
