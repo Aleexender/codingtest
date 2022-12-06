ab69bc1e06a13b0ea3e0a14962e9e4db93c3cebf
Alex Kim <alexkjh2011@gmail.com>
2022-12-07 02:18:24 +0900

    put a for() and print

--- src/Math.java
+++ src/Math.java
@@ -4,3 +4,14 @@ public class Math {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = scanner.nextInt();
+
+
+        for (int i = 0; i < count; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int total = A+B;
+            System.out.println(total);
+        }
+
+    }
+}
