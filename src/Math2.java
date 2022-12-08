517f6028864cf40882b9336409ad1b95bd0975e6
Alex Kim <alexkjh2011@gmail.com>
2022-12-08 23:04:25 +0900

    put a while and print total

--- src/Math2.java
+++ src/Math2.java
@@ -4,3 +4,15 @@ import java.util.Scanner;
 public class Math2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextInt()){
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int total = A+B;
+
+            System.out.println(total);
+
+        }
+
+    }
+
