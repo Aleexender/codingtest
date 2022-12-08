6243f1728f2da7b7a5ec8d2b82ac1e7012d8ccf3
Alex Kim <alexkjh2011@gmail.com>
2022-12-08 23:05:02 +0900

    make a new class which is AxB  and math method and print

--- src/Math2.java
+++ src/Math2.java
@@ -16,3 +16,15 @@ public class Math2 {
 
     }
 
+    public static class AxB {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int total = A * B;
+
+            System.out.println(total);
+        }
+    }
+}
+
