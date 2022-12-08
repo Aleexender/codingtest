dcb359353deacd4fc15b9a676418afe9afff80e2
Alex Kim <alexkjh2011@gmail.com>
2022-12-09 01:55:53 +0900

    put a while() and linked with Scanner

--- src/Math3.java
+++ src/Math3.java
@@ -4,3 +4,8 @@ import java.util.Scanner;
 public class Math3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextInt()){
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int total = A+B;
