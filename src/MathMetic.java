9a2a0e9476da904390d9df2662c0d9b70efb212e
Alex Kim <alexkjh2011@gmail.com>
2022-12-10 00:53:08 +0900

    make a scanner

--- src/MathMetic.java
+++ src/MathMetic.java
@@ -2,3 +2,6 @@ import java.util.Scanner;
 
 public class MathMetic {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
