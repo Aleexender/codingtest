33108cede2371a808643b129b307a1987b5097c1
Alex Kim <alexkjh2011@gmail.com>
2022-12-12 04:02:27 +0900

    make a scanner and instances

--- src/TheBiggest.java
+++ src/TheBiggest.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class TheBiggest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arrnum = new int[9];
+        final int repeatNum = 9;
+        int max = 0;
+        int index = 0;
