4206400d638e506db451aa8b692cf07f1032d705
Alex Kim <alexkjh2011@gmail.com>
2022-12-12 04:02:58 +0900

    put a for() and if() to put in array and compare the biggest num

--- src/TheBiggest.java
+++ src/TheBiggest.java
@@ -7,3 +7,12 @@ public class TheBiggest {
         final int repeatNum = 9;
         int max = 0;
         int index = 0;
+
+        for (int i = 0; i < repeatNum; i++) { // 어레이에 대입해주는 for문
+            int input = scanner.nextInt(); // 값 입력
+            arrnum[i] = input;// 입력한 값을 i번째 인덱스 arrnum 에 대입
+            if (arrnum[i] > max) {
+                max = arrnum[i]; //arrnum의 값은 max로 넘어감
+                index = i+1; // index 값을 비교하는게 아니라 몇번째 인지 확인하는거기 때문에 +1
+            }
+        }
