d8738aea6c393df0880b8dc8ceb6fc51c1ed8a46
Alex Kim <alexkjh2011@gmail.com>
2022-12-13 02:21:22 +0900

    put for() to get biggest number

--- src/Average.java
+++ src/Average.java
@@ -8,3 +8,8 @@ public class Average {
         double logic = 0;
         int[] score = new int[subject];
         double average = 0;
+
+        for (int i = 0; i < subject; i++) { // 최대값을 구하는 반복문
+            score[i] = scanner.nextInt();
+            maxScore = java.lang.Math.max(score[i], maxScore);
+        }
