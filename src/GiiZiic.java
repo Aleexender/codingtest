038b54531f31e6956a77d6a6408b504e18ea62cd
Alex Kim <alexkjh2011@gmail.com>
2022-12-14 02:10:55 +0900

    make scanner and instance in GiiZiic

--- src/GiiZiic.java
+++ src/GiiZiic.java
@@ -2,3 +2,5 @@ import java.util.Scanner;
 
 public class GiiZiic {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
