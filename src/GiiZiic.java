569776e71d0ea499aa0880d4fb6d92f314c980cc
Alex Kim <alexkjh2011@gmail.com>
2022-12-14 02:11:17 +0900

    put for() to decrease the num

--- src/GiiZiic.java
+++ src/GiiZiic.java
@@ -4,3 +4,9 @@ public class GiiZiic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
+
+        for (int i = 0; i < num; num--) {
+            System.out.println(num);
+        }
+    }
+}
