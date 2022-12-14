9a9863bedaded3debff84666d067b79fcf7835bc
Alex Kim <alexkjh2011@gmail.com>
2022-12-15 03:58:20 +0900

    make Scanner and instance in BiggestLowest

--- src/BiggestLowest.java
+++ src/BiggestLowest.java
@@ -2,3 +2,7 @@ import java.util.Scanner;
 
 public class BiggestLowest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int max = -1000000; // 가장 작은수 부터 시작
+        int min = 1000000; // 가장 큰수 부터 시작
