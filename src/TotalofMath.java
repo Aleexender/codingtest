37693fe0a054d4933b61af6b51a417acac796f92
Alex Kim <alexkjh2011@gmail.com>
2022-12-16 02:58:48 +0900

    make Scanner and put int and String instances

--- src/TotalofMath.java
+++ src/TotalofMath.java
@@ -2,3 +2,10 @@ import java.util.Scanner;
 
 public class TotalofMath {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int times = scanner.nextInt();
+        String eachnum = scanner.next();
+
+        String[] array = eachnum.split("");
+        int total = 0;
+
