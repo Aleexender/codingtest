dc419373e0f579ec8dc50d93468cc7f21acb8c03
Alex Kim <alexkjh2011@gmail.com>
2022-12-16 02:59:05 +0900

    put try catch and print it

--- src/TotalofMath.java
+++ src/TotalofMath.java
@@ -9,3 +9,14 @@ public class TotalofMath {
         String[] array = eachnum.split("");
         int total = 0;
 
+        try {
+            for (int i = 0; i < times; i++) {
+                total += Integer.parseInt(array[i]);
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        }
+        System.out.println(total);
+    }
+}
