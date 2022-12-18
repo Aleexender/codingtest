7f02a19f0f7bb6d5ed35561b99edbfef1b70a9d5
Alex Kim <alexkjh2011@gmail.com>
2022-12-18 17:21:23 +0900

    put instances and scanner

--- src/FindAlpha.java
+++ src/FindAlpha.java
@@ -3,3 +3,5 @@ import java.util.Scanner;
 
 public class FindAlpha {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] alpha = new int[26];
