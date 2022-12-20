cda922dde7a64d59a7a9931898baf51ac18cd025
Alex Kim <alexkjh2011@gmail.com>
2022-12-20 18:02:22 +0900

    put if()

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -8,3 +8,11 @@ public class AlarmClock {
         final int getUpEarly = 45;
 
         int clock = min - 45;
+        if (min < getUpEarly) {
+            hour -= 1;
+            min += 60;
+
+            if (hour < 0) {
+                hour += 24;
+            }
+        }
