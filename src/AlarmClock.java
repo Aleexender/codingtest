f1bc7e03a626997fa701302d592b73f00933adfe
Alex Kim <alexkjh2011@gmail.com>
2022-12-20 18:02:03 +0900

    make scanner and instances

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -1,3 +1,10 @@
 import java.util.Scanner;
 
 public class AlarmClock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hour = scanner.nextInt();
+        int min = scanner.nextInt();
+        final int getUpEarly = 45;
+
+        int clock = min - 45;
