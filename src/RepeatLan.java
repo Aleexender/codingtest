f631e8cb402832d24ba4dc75e96c5ebf4c1b2ba4
Alex Kim <alexkjh2011@gmail.com>
2022-12-22 00:38:29 +0900

    put for() and put int and String

--- src/RepeatLan.java
+++ src/RepeatLan.java
@@ -4,3 +4,8 @@ public class RepeatLan {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = scanner.nextInt();
+
+        for (int j = 0; j < count; j++) {
+            int time = scanner.nextInt();
+            String language = scanner.next();
+
