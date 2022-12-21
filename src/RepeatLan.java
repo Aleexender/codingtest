cf03d6b1b52da7ceb3825301a902f6e3c13b64c1
Alex Kim <alexkjh2011@gmail.com>
2022-12-22 00:37:54 +0900

    put a scanner and instance

--- src/RepeatLan.java
+++ src/RepeatLan.java
@@ -2,3 +2,5 @@ import java.util.Scanner;
 
 public class RepeatLan {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
