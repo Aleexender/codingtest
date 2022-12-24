a482d7e53cb7b838b3cfbc0234e0608cba022504
Alex Kim <alexkjh2011@gmail.com>
2022-12-24 14:32:25 +0900

    put constructor and instances

--- src/AgeRank.java
+++ src/AgeRank.java
@@ -3,3 +3,10 @@ import java.util.Scanner;
 
 public class AgeRank {
     static class AgeName implements Comparable<AgeName> {
+        String name;
+        int age;
+
+        AgeName(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
