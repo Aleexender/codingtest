192af7fe5fc6112ce5bcf7ebd65cb04b97a13fc4
Alex Kim <alexkjh2011@gmail.com>
2022-12-24 14:33:53 +0900

    put a scanner and instance and array

--- src/AgeRank.java
+++ src/AgeRank.java
@@ -26,3 +26,6 @@ public class AgeRank {
         }
 
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int count = scanner.nextInt();
+            AgeName[] ageNames = new AgeName[count];
