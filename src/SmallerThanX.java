8f813e9953cbd837ac9efc6959804426f9ac0fc4
Alex Kim <alexkjh2011@gmail.com>
2022-12-26 22:48:56 +0900

    put instances and array

--- src/SmallerThanX.java
+++ src/SmallerThanX.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class SmallerThanX {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int standard = scanner.nextInt();
+        int[] num = new int[count];
