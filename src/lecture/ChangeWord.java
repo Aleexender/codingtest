195e5514db6aac2ba2c8c7ac1bffb69823063a9d
Alex Kim <alexkjh2011@gmail.com>
2022-12-28 00:33:50 +0900

    put Scanner and instances

--- src/lecture/ChangeWord.java
+++ src/lecture/ChangeWord.java
@@ -4,3 +4,9 @@ import java.util.Scanner;
 
 public class ChangeWord {
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        StringBuilder answer = new StringBuilder();
+
