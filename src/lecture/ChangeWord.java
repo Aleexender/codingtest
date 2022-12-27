1478d06c054dea7994f4d2143a8358ed30157c3e
Alex Kim <alexkjh2011@gmail.com>
2022-12-28 00:34:28 +0900

    put for() and inside put if() & else to classify word is uppercase or lowercase

--- src/lecture/ChangeWord.java
+++ src/lecture/ChangeWord.java
@@ -10,3 +10,12 @@ public class ChangeWord {
         String input = scanner.next();
         StringBuilder answer = new StringBuilder();
 
+        for (int i = 0; i < input.length(); i++) {
+            if ((input.charAt(i) + "").toUpperCase().equals(input.charAt(i) + "")) {
+//                input += (input.charAt(i) + "").toLowerCase();
+                answer.append(String.valueOf(input.charAt(i)).toLowerCase());
+            }else{
+                answer.append(String.valueOf(input.charAt(i)).toUpperCase());
+            }
+
+        }
