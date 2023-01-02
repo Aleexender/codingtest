e92ff3af50e41621912ddfadf76eece29724a078
Alex Kim <alexkjh2011@gmail.com>
2023-01-03 00:45:16 +0900

    make a recursive method

--- src/Factorial.java
+++ src/Factorial.java
@@ -7,3 +7,17 @@ public class Factorial {
         System.out.println(fun(input));
     }
 
+
+    static int fun(int input) {
+        //base case
+        if (input == 0) {
+            return 1;
+        } else if (input == 1) {
+            return 1;
+        }
+
+
+        //recursive case -> 10에서 시작, 9를 알아야 하고 8을 알아야하니...1을 알야하 한다...
+        return input * fun(input - 1);
+    }
+}
