581cd09c1af08d15cd5577ca2af4823acf5e8b79
Alex Kim <alexkjh2011@gmail.com>
2023-01-03 00:44:54 +0900

    make a Scanner and int and print

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.println(fun(input));
+    }
+
