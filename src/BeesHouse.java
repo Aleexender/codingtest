93bd25e7f02de1216f158f0768f77a1d18ff3d75
Alex Kim <alexkjh2011@gmail.com>
2023-01-06 02:32:34 +0900

    put Scanner and instances

--- src/BeesHouse.java
+++ src/BeesHouse.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class BeesHouse {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int sectionLogic = 2;//8 ,20
+        final int sectiononeend = 7;
+        int previousSection = sectionLogic - 1;
+        int answer = 0;
