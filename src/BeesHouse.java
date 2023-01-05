bbfb83c3aa5f22c0a65f3a3f0b39004b0b2515b4
Alex Kim <alexkjh2011@gmail.com>
2023-01-06 02:33:18 +0900

    put for() and if to multiply and print answer

--- src/BeesHouse.java
+++ src/BeesHouse.java
@@ -16,3 +16,17 @@ public class BeesHouse {
             System.out.println("2");
             return;
         }
+
+
+        for (int i = 1; i < 100000000; i++) {
+            sectionLogic = sectionLogic + 6 * i; // 누적합을 할려면 똑같은 로컬변수를 향해하한다.
+            int insidenum = 6 * (i + 1);
+
+            if (sectionLogic <= input && sectionLogic + insidenum - 1 >= input) {
+                answer = i + 2;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
