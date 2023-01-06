6186d6370dee467c669a2a87d91dd5da3e6c670e
Alex Kim <alexkjh2011@gmail.com>
2023-01-07 01:30:36 +0900

    make a scanner and put int and print it

--- src/Pibonachi.java
+++ src/Pibonachi.java
@@ -2,3 +2,6 @@ import java.util.Scanner;
 
 public class Pibonachi {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(fun(num));
