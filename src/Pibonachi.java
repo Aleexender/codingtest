e509b09d798b449d8fec7b53fe347d8ce3c6b5d3
Alex Kim <alexkjh2011@gmail.com>
2023-01-07 01:31:17 +0900

    this part is base case which is we can know directly

--- src/Pibonachi.java
+++ src/Pibonachi.java
@@ -9,3 +9,10 @@ public class Pibonachi {
     }
 // 어떤 문제를 풀지 -> base case -> recursive
     static int fun( int num) {
+        //base case -> 떠넘기냐 안떠넘기냐, 안떠넘기면 base case(떠넘기지 않아도 정답을 바로 구할수 있는것)
+        if (num == 0) {
+            return 0;
+        }else if (num == 1) {
+            return 1;
+        }
+
