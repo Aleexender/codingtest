397f3dcfb7739071380e4464d03f57fc766c9182
Alex Kim <alexkjh2011@gmail.com>
2023-01-07 01:30:55 +0900

    make a other method

--- src/Pibonachi.java
+++ src/Pibonachi.java
@@ -5,3 +5,7 @@ public class Pibonachi {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
         System.out.println(fun(num));
+
+    }
+// 어떤 문제를 풀지 -> base case -> recursive
+    static int fun( int num) {
