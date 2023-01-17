5a350ed153114d9af2847c87911a2714156595be
Alex Kim <alexkjh2011@gmail.com>
2023-01-18 03:48:03 +0900

    make a Scanner and instance variables and list

--- src/RepresentNum.java
+++ src/RepresentNum.java
@@ -2,3 +2,9 @@ import java.util.*;
 
 public class RepresentNum {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int five = 5;
+        int addAll = 0;
+        int average = 0;
+        List list = new ArrayList();
+        int[] nums = new int[five];
