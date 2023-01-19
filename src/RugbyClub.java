30e50e25dd2986f6735d0a12e998846fd0860f05
Alex Kim <alexkjh2011@gmail.com>
2023-01-20 03:14:56 +0900

    put a scanner and instances variables and final instances variable

--- src/RugbyClub.java
+++ src/RugbyClub.java
@@ -2,3 +2,11 @@ import java.util.Scanner;
 
 public class RugbyClub {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        int age = 0;
+        int weight = 0;
+        final int STANDARD_OF_WEIGHT = 80;
+        final int STANDARD_OF_AGE = 17;
+        final String JUNIOR = " Junior";
+        final String SENIOR = " Senior";
