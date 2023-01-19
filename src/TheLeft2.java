46369a3300535abbf4e2bb086649503d102c5642
Alex Kim <alexkjh2011@gmail.com>
2023-01-19 13:57:55 +0900

    put instance variables in TheLeft2 class

--- src/TheLeft2.java
+++ src/TheLeft2.java
@@ -4,3 +4,7 @@ import java.util.Set;
 
 public class TheLeft2 {
     public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+        Set<Integer> hashSet = new HashSet<>();
+        final int repeat = 10;
+        int count = 0;
