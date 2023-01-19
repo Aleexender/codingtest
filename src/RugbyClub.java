2edfce0a9d24dd0138f21bb6c27a781b446d8ba5
Alex Kim <alexkjh2011@gmail.com>
2023-01-20 03:15:17 +0900

    put for() and put if and do print

--- src/RugbyClub.java
+++ src/RugbyClub.java
@@ -10,3 +10,21 @@ public class RugbyClub {
         final int STANDARD_OF_AGE = 17;
         final String JUNIOR = " Junior";
         final String SENIOR = " Senior";
+
+        while (scanner.hasNext()) {
+            name = scanner.next();
+            age = scanner.nextInt();
+            weight = scanner.nextInt();
+
+            if (age == 0 || weight == 0) {
+                break;
+            }
+
+            if (age > STANDARD_OF_AGE || weight >= STANDARD_OF_WEIGHT) {
+                System.out.println(name + SENIOR);
+            } else {
+                System.out.println(name + JUNIOR);
+            }
+        }
+    }
+}
