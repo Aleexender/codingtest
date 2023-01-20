38e6944713c40bb040620eff6fb573d19f3bce1b
Alex Kim <alexkjh2011@gmail.com>
2023-01-21 03:31:34 +0900

    make a Scanner and instance varivables

--- src/SayingAh.java
+++ src/SayingAh.java
@@ -2,3 +2,7 @@ import java.util.Scanner;
 
 public class SayingAh {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String jInput = scanner.next();
+        String docInput = scanner.next();
+
