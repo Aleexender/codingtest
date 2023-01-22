e950e7b5c0dceb297b9312f066d5d5902a296ea9
Alex Kim <alexkjh2011@gmail.com>
2023-01-23 04:44:59 +0900

    Make Scanner and put List to put variables

--- src/WhoIsInMiddle.java
+++ src/WhoIsInMiddle.java
@@ -2,3 +2,5 @@ import java.util.*;
 
 public class WhoIsInMiddle {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> list = new ArrayList();
