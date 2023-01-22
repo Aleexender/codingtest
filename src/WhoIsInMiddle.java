9154319c40e1fd3eb465a08463dc3705d446543b
Alex Kim <alexkjh2011@gmail.com>
2023-01-23 04:45:29 +0900

    make for and sorting it to arrange it

--- src/WhoIsInMiddle.java
+++ src/WhoIsInMiddle.java
@@ -4,3 +4,13 @@ public class WhoIsInMiddle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Integer> list = new ArrayList();
+
+        for (int i = 0; i < 3; i++) {
+            list.add(scanner.nextInt());
+        }
+        Collections.sort(list);
+
+        System.out.println(list.get(1));
+
+    }
+}
