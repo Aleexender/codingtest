59c36ef3a1e998eb83ad525970da0d99a8d5a0ee
Alex Kim <alexkjh2011@gmail.com>
2023-01-24 04:01:56 +0900

    make a Scanner and put String[]

--- src/LineNumber.java
+++ src/LineNumber.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 
 public class LineNumber {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int time = Integer.parseInt(scanner.nextLine());
+        String[] qwe = new String[time];
