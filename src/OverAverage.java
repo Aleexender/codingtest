bccb54e02961a6c68038cf8d3e5aef6f3bcfc8d7
Alex Kim <alexkjh2011@gmail.com>
2023-01-25 03:49:21 +0900

    put a for to run testCase

--- src/OverAverage.java
+++ src/OverAverage.java
@@ -4,3 +4,12 @@ public class OverAverage {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int testCase = scanner.nextInt();
+
+        for (int i = 0; i < testCase; i++) {
+            int students = scanner.nextInt();
+            int score[] = new int[students];
+            int average = 0;
+            int totalScore = 0;
+            double whoOverScore = 0;
+            double answer = 0;
+
