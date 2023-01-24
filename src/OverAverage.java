d32b0e73f4ac9fd161c5a6a6f1ffebb40cab884d
Alex Kim <alexkjh2011@gmail.com>
2023-01-25 03:48:44 +0900

    make scanner and main fields to run for()

--- src/OverAverage.java
+++ src/OverAverage.java
@@ -2,3 +2,5 @@ import java.util.Scanner;
 
 public class OverAverage {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCase = scanner.nextInt();
