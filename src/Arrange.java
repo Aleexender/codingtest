2ff4933738310796052aba5d0443599b7755eafa
Alex Kim <alexkjh2011@gmail.com>
2023-01-27 16:52:34 +0900

    sorting details

--- src/Arrange.java
+++ src/Arrange.java
@@ -33,3 +33,24 @@ public class Arrange {
 //            this.y = y;
 //        }
 
+        //y좌표의 값이 낮으면 우선순위가 ]\낮다 => 숫자가 낮으면 빨리 빠져나간다
+        //x좌표의 값이 낮음면 우선순위가 낮다. => ""
+        @Override
+        public int compareTo(Setting o) {
+            if (this.y < o.y) {
+                return -1;
+            } else if (this.y > o.y) {
+                return +1;
+            } else {
+                if (this.x < o.x) {
+                    return -1;
+                } else if (this.x > o.x) {
+                    return +1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    }
+
+}
