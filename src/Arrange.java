81641846080cd61b9987d32669c8c00265b96c43
Alex Kim <alexkjh2011@gmail.com>
2023-01-27 16:51:39 +0900

    put for() to put Setting class and put to array

--- src/Arrange.java
+++ src/Arrange.java
@@ -7,3 +7,12 @@ public class Arrange {
         int count = scanner.nextInt();
         Setting[] inputArray = new Setting[count];
 
+        for (int i = 0; i < count; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            Setting setting = new Setting();
+            setting.y = y;
+            setting.x = x;
+            inputArray[i] = setting;
+//            inputArray[i] = new Setting(x, y); // x,y
+        }
