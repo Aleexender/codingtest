a050e4b03bdb5595f6597d7198ad832ff8397604
Alex Kim <alexkjh2011@gmail.com>
2023-01-27 16:51:03 +0900

    put instance variables

--- src/Arrange.java
+++ src/Arrange.java
@@ -3,3 +3,7 @@ import java.util.Scanner;
 
 public class Arrange {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        Setting[] inputArray = new Setting[count];
+
