699f7b379a82912cf588ca9c98562bde25d09c23
Alex Kim <alexkjh2011@gmail.com>
2023-01-29 00:52:32 +0900

    put instances

--- src/FindSue.java
+++ src/FindSue.java
@@ -2,3 +2,10 @@ import java.util.Scanner;
 
 public class FindSue {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        String change = scanner.next();
+        String[] t = new String[N];
+        t = change.split(",");
+        int[] num = new int[N];
