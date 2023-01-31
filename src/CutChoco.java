e2c6fa8fbc83aa6aa94c1349e1073b81746bae86
Alex Kim <alexkjh2011@gmail.com>
2023-02-01 00:15:37 +0900

    put a scanner and instances and put logic and print

--- src/CutChoco.java
+++ src/CutChoco.java
@@ -3,3 +3,10 @@ import java.util.Scanner;
 
 public class CutChoco {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        int count = 0;
+             System.out.println(row * col - 1);
+    }
+}
