45ca4078a1e41dfdeba10c55610f06da652f7b64
Alex Kim <alexkjh2011@gmail.com>
2023-02-02 04:48:05 +0900

    put while() and put instance variables

--- src/DobbyEnglish.java
+++ src/DobbyEnglish.java
@@ -5,3 +5,11 @@ public class DobbyEnglish {
         Scanner scanner = new Scanner(System.in);
 
 
+        while (scanner.hasNext()) {
+            String alpha = scanner.next();
+            String input = scanner.nextLine();
+            input = input.toLowerCase();
+            alpha = alpha.toLowerCase();
+            String[] change = input.split("");
+            int count = 0;
+
