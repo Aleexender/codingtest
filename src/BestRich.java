39fa43252b2bee2ae3145a28a529c79bcf211df0
Alex Kim <alexkjh2011@gmail.com>
2023-02-03 16:13:22 +0900

    put main and put Scanner and BigIntegers

--- src/BestRich.java
+++ src/BestRich.java
@@ -2,3 +2,7 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class BestRich {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger n = scanner.nextBigInteger();
+        BigInteger m = scanner.nextBigInteger();
