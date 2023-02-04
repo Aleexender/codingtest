1561c597b2980e08fa5aebb384091aa66a20e439
Alex Kim <alexkjh2011@gmail.com>
2023-02-04 22:00:31 +0900

    put Scanner instance and put 2 BigIntegers and print it

--- src/BigNum.java
+++ src/BigNum.java
@@ -2,3 +2,11 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class BigNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger input = scanner.nextBigInteger();
+        BigInteger input2 = scanner.nextBigInteger();
+        System.out.println(input2.add(input));
+
+    }
+}
