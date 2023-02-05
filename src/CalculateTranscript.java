fcf020312ac18e817c2993fc0423ca1882e12bf7
Alex Kim <alexkjh2011@gmail.com>
2023-02-05 19:52:13 +0900

    put if and else if to array it

--- src/CalculateTranscript.java
+++ src/CalculateTranscript.java
@@ -4,3 +4,14 @@ public class CalculateTranscript {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
+        if (input.equals("A+")) {
+            System.out.println("4.3");
+        } else if (input.equals("A0")) {
+            System.out.println("4.0");
+        } else if (input.equals("A-")) {
+            System.out.println("3.7");
+        } else if (input.equals("B+")) {
+            System.out.println("3.3");
+        }
+    }
+}
