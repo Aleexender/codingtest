b7f8d58234ca3a17d9d4ec1c6ed59939a0dae019
Alex Kim <alexkjh2011@gmail.com>
2023-02-06 04:19:16 +0900

    put Scanner and instance variables

--- src/CardGame.java
+++ src/CardGame.java
@@ -2,3 +2,7 @@ import java.util.Scanner;
 
 public class CardGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int time = 5;
+        int total = 0;
+
