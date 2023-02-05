ac286e6307189690a16096597b6696001ec8ef60
Alex Kim <alexkjh2011@gmail.com>
2023-02-05 19:51:58 +0900

    put instance variables and scanner

--- src/CalculateTranscript.java
+++ src/CalculateTranscript.java
@@ -2,3 +2,5 @@ import java.util.Scanner;
 
 public class CalculateTranscript {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
