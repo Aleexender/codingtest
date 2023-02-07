a0abe458db5e88eb2c24d6444ab406f448fb37fa
Alex Kim <alexkjh2011@gmail.com>
2023-02-07 22:32:33 +0900

    put while() and put if inside of it and divide and print

--- src/BreakEvenPoint.java
+++ src/BreakEvenPoint.java
@@ -8,3 +8,24 @@ public class BreakEvenPoint {
         int sellPrice = scanner.nextInt();
         int quantity = 0;
 
+
+        while (true) {
+            if (sellPrice <= originalCost) {
+                System.out.println(-1);
+                break;
+            }
+//            quantity++;
+//            addProduct += sellPrice;
+//            addProduct2 += originalCost;
+//            if (fixedCost + addProduct2 < addProduct) {
+//                System.out.println(quantity);
+//                break;
+//            }
+            quantity = fixedCost / (sellPrice - originalCost);
+            System.out.println(quantity+1);
+            break;
+//            if (total < sellPrice) {
+//                System.out.println(quantity);
+//                break;
+//            }
+
