15861a347861da8325fb045fb0fbf732e3ceb114
Alex Kim <alexkjh2011@gmail.com>
2023-02-07 22:32:09 +0900

    put Scanner and instance variables

--- src/BreakEvenPoint.java
+++ src/BreakEvenPoint.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class BreakEvenPoint {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int fixedCost = scanner.nextInt();
+        int originalCost = scanner.nextInt();
+        int sellPrice = scanner.nextInt();
+        int quantity = 0;
+
