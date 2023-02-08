76e3a8a37cea8f03c73f657cb21c1d7d5cf2ace1
Alex Kim <alexkjh2011@gmail.com>
2023-02-08 21:36:25 +0900

    while() and if() and instance variable and put other while()

--- src/Chicken.java
+++ src/Chicken.java
@@ -4,3 +4,19 @@ public class Chicken {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
+
+            if (n == 0 && k == 0) {
+                break;
+            }
+            int chicken = 0;
+            chicken += n;
+//            int maxCoupon = k;
+//            int coupon = chicken / maxCoupon;
+//            int restStamp = coupon / maxCoupon;
+            while (n >= k) {
+                n = n / k;
+                chicken += n;
+            }
