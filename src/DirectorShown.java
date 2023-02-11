10003fa4dbf6f155a97790e4cc3284a22f8bb1b0
Alex Kim <alexkjh2011@gmail.com>
2023-02-12 02:45:38 +0900

    put instance variables and Scanner

--- src/DirectorShown.java
+++ src/DirectorShown.java
@@ -2,3 +2,10 @@ import java.util.Scanner;
 
 public class DirectorShown {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int movie = 666;
+        int count = 0;
+    }
+}
