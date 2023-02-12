ed00b1ab62a25b95759e4280209ee7c8617b9d3b
Alex Kim <alexkjh2011@gmail.com>
2023-02-13 03:24:01 +0900

    Create ArrangeNum.java

---
+++ src/ArrangeNum.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ArrangeNum {
+    public static void main(String[] args) throws IOException {
