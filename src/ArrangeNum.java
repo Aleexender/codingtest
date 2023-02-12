b37f363613bc41e8e81ffd4ff6ed1e3a5743c695
Alex Kim <alexkjh2011@gmail.com>
2023-02-13 03:24:40 +0900

    put Bufferedreader and Writer , Moreover put instance variables

--- src/ArrangeNum.java
+++ src/ArrangeNum.java
@@ -6,3 +6,11 @@ import java.util.Scanner;
 
 public class ArrangeNum {
     public static void main(String[] args) throws IOException {
+        //   Scanner scanner = new Scanner(System.in);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int quantity = Integer.parseInt(bufferedReader.readLine());
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+        int[] array = new int[quantity];
+
