bee3a97fe002b4ca9754fc2b95e6735bd5d446c6
Alex Kim <alexkjh2011@gmail.com>
2023-02-15 22:09:38 +0100

    put while() and put instance variables and put if() to limit and put for() to make output

--- src/WinterOhWinter.java
+++ src/WinterOhWinter.java
@@ -3,3 +3,16 @@ import java.util.Scanner;
 public class WinterOhWinter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String[] splitWord = input.split("");
+
+            if (input.equals("***")) {
+                break;
+            }
+
+            for (int i = splitWord.length-1; i >= 0; i--) {
+                String output = splitWord[i];
+                System.out.print(output);
+
+            }
