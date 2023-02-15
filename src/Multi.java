d821dec89f4ed525897025600347a7472d85301e
Alex Kim <alexkjh2011@gmail.com>
2023-02-15 01:33:06 +0100

    put instance variables

--- src/Multi.java
+++ src/Multi.java
@@ -1,3 +1,9 @@
 import java.util.Scanner;
 
 public class Multi {//2588
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String multiNum = scanner.next();
+        String[] split = multiNum.split("");
+        int result = 0;
