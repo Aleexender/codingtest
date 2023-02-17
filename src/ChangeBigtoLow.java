fd49ef8ed0490ea61e5730f72bc78458450c3c86
Alex Kim <alexkjh2011@gmail.com>
2023-02-17 15:30:41 +0100

    make for() and put if else inside also print answer

--- src/ChangeBigtoLow.java
+++ src/ChangeBigtoLow.java
@@ -6,3 +6,14 @@ public class ChangeBigtoLow {
         String input = scanner.next();
         char[] changeEach = input.toCharArray();
         StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (changeEach[i] == Character.toLowerCase(changeEach[i])) {
+                answer.append(Character.toUpperCase(changeEach[i]));
+            } else {
+               answer.append(Character.toLowerCase(changeEach[i]));
+            }
+        }
+        System.out.println(answer);
+    }
+}
