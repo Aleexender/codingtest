a3b2198f1ce5a91d699ab94acc0ae56e65752621
Alex Kim <alexkjh2011@gmail.com>
2023-02-18 19:26:04 +0100

    make for to put array using for()

--- src/ArrangeNumbersBronze.java
+++ src/ArrangeNumbersBronze.java
@@ -7,3 +7,7 @@ public class ArrangeNumbersBronze {
         Scanner scanner = new Scanner(System.in);
         int[] arr = new int[3];
 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
