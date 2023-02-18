4d1d1329c18cded7e8b6f8b880f26a850f13b3e0
Alex Kim <alexkjh2011@gmail.com>
2023-02-18 19:25:44 +0100

    make instance variables

--- src/ArrangeNumbersBronze.java
+++ src/ArrangeNumbersBronze.java
@@ -4,3 +4,6 @@ import java.util.Scanner;
 
 public class ArrangeNumbersBronze {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[3];
+
