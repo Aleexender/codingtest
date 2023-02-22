ff23dfbf5dd961f089f51b8631512318564508c1
Alex Kim <alexkjh2011@gmail.com>
2023-02-22 01:12:32 +0100

    put instance variables

--- src/CheckInManage.java
+++ src/CheckInManage.java
@@ -2,3 +2,6 @@ import java.util.Scanner;
 
 public class CheckInManage {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+
