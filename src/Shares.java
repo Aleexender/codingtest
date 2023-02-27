4928278412bc45c5783c9c795e0e33fb7ea27325
Alex Kim <alexkjh2011@gmail.com>
2023-02-27 01:39:30 +0100

    put while() and put variables and print

--- src/Shares.java
+++ src/Shares.java
@@ -4,3 +4,12 @@ public class Shares {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         final int CHIEF = 1;
+        while (scanner.hasNext()) {
+
+            int n = scanner.nextInt() + CHIEF;
+            int s = scanner.nextInt();
+
+            System.out.println(s / n);
+        }
+    }
+}
