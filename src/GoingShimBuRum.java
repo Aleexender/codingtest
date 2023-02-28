3ea2eae757da4df0fdae27d3762c1a5df34c0756
Alex Kim <alexkjh2011@gmail.com>
2023-02-28 23:51:55 +0100

    put for() and int and if and finally print it

--- src/GoingShimBuRum.java
+++ src/GoingShimBuRum.java
@@ -8,3 +8,17 @@ public class GoingShimBuRum {
         int changeToSec = 0;
 
 
+        for (int i = 0; i < 4; i++) {
+            input += scanner.nextInt();
+        }
+        int min = input / 60;
+        System.out.println(min);
+
+        changeToSec = min * 60;
+
+        if (input-changeToSec < 60) {
+            leftSec = input - changeToSec;
+        }
+        System.out.println(leftSec);
+    }
+}
