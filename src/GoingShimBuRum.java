a0e7e0b58b622040717cdabc5cffa364a906148f
Alex Kim <alexkjh2011@gmail.com>
2023-02-28 23:51:38 +0100

    put scanners and instance variables

--- src/GoingShimBuRum.java
+++ src/GoingShimBuRum.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class GoingShimBuRum {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        int leftSec = 0;
+        int changeToSec = 0;
+
+
