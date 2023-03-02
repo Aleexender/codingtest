f67b22bf13c641e58253500fa7a3dbcf9f5b6901
Alex Kim <alexkjh2011@gmail.com>
2023-03-02 17:49:36 +0100

    make instance variables and make List

--- src/NumberArrange.java
+++ src/NumberArrange.java
@@ -4,3 +4,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class NumberArrange {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        List<Integer> list = new ArrayList();
+
