eb35a39129317a7ff34ef267f2acf5ec1025edbc
Alex Kim <alexkjh2011@gmail.com>
2023-03-03 23:44:00 +0100

    make fun method and put if() and return recursive case

--- src/CutRanLine.java
+++ src/CutRanLine.java
@@ -12,3 +12,22 @@ public class CutRanLine {
             newRan = scanner.nextInt();
             fun(ranQuantity,divideRan,newRan);
         }
+
+
+
+    }
+public static int plusRan = 1;
+
+    public static void fun(int ranQuantity, int ranResult, int ran) {
+        // base case
+        plusRan++;
+        if (ranQuantity == ranResult) {
+            return;
+        }
+
+
+        //recursive case
+        int quantity = ran / plusRan;
+        fun(ranQuantity + plusRan, ranResult, ran);
+    }
+}
