002fd59cbfc9e1c4a616bf079930f83863ce9462
Alex Kim <alexkjh2011@gmail.com>
2023-03-03 23:43:26 +0100

    make main method and instances and call fun()

--- src/CutRanLine.java
+++ src/CutRanLine.java
@@ -1,3 +1,14 @@
 import java.util.Scanner;
 
 public class CutRanLine {
+    public static int newRan = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ranQuantity = scanner.nextInt();
+        int divideRan = scanner.nextInt();
+        int newRan = 0;
+
+        for (int i = 0; i < ranQuantity; i++) {
+            newRan = scanner.nextInt();
+            fun(ranQuantity,divideRan,newRan);
+        }
