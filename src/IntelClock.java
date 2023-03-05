f01211cc68cd0ce58ab431be28566ccb2d4c14eb
Alex Kim <alexkjh2011@gmail.com>
2023-03-05 22:49:11 +0100

    and add it and print

--- src/IntelClock.java
+++ src/IntelClock.java
@@ -8,3 +8,18 @@ public class IntelClock {
         int sec = scanner.nextInt();
         int add = scanner.nextInt();
 
+        min += add / 60;
+        sec += add % 60;
+
+        min += sec / 60;
+        sec = sec % 60;
+
+        hour += min / 60;
+        min = min % 60;
+
+        hour = hour % 24;
+
+
+        System.out.println(hour +" " + min + " "+ sec);
+    }
+}
