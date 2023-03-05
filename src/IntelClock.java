b5da951db7ada7f7a0e7cd7f200b4f3ca4a9e853
Alex Kim <alexkjh2011@gmail.com>
2023-03-05 22:47:47 +0100

    make instance variables

--- src/IntelClock.java
+++ src/IntelClock.java
@@ -1,3 +1,10 @@
 import java.util.Scanner;
 
 public class IntelClock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hour = scanner.nextInt();
+        int min = scanner.nextInt();
+        int sec = scanner.nextInt();
+        int add = scanner.nextInt();
+
