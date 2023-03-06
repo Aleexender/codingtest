c85d368c2d8b2903cbd5fd30e3dc40443043f806
Alex Kim <alexkjh2011@gmail.com>
2023-03-06 17:22:05 +0100

    make instance variables

--- src/MakeEnerGram.java
+++ src/MakeEnerGram.java
@@ -2,3 +2,10 @@ import java.util.Scanner;
 
 public class MakeEnerGram {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String wordDuplicate = scanner.nextLine();
+        String[] divideWord = word.split("");
+        String[] divideWord2 = word.split("");
+        int count = 0;
+
