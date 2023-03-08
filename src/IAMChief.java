857e5eeb0e9cb396cbab90e7f48613fb8f0e8042
Alex Kim <alexkjh2011@gmail.com>
2023-03-08 02:17:44 +0100

    make instances

--- src/IAMChief.java
+++ src/IAMChief.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class IAMChief {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        int max = 0;
+        int num = 0;
+
