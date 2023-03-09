bbdf2ca9c2be410368cb5d3e041f1b8003bb0e41
Alex Kim <alexkjh2011@gmail.com>
2023-03-10 00:16:37 +0100

    make instances variable

--- src/PhoneCallCost.java
+++ src/PhoneCallCost.java
@@ -2,3 +2,10 @@ import java.util.Scanner;
 
 public class PhoneCallCost {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int time = scanner.nextInt();
+        int minPayment = 0;
+        int yongPayment = 0;
+        for (int i = 0; i < time; i++) {
+            int phoneSec = scanner.nextInt();
+
