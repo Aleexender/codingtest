7141770932a5ce9db08db8ed8e79bd9b91ffd87c
Alex Kim <alexkjh2011@gmail.com>
2023-03-12 22:26:35 +0100

    put while() and put for() to conclude the answer

--- src/CountOfAEIOU.java
+++ src/CountOfAEIOU.java
@@ -5,3 +5,21 @@ public class CountOfAEIOU {
         Scanner scanner = new Scanner(System.in);
         char[] aeiou = {'a', 'e', 'i', 'o', 'u'};
 
+        while (true) {
+            String input = scanner.nextLine();
+
+
+            if (input.equals("#")) {
+                break;
+            }
+
+            char[] divide = input.toLowerCase().toCharArray();
+            int count = 0;
+
+            for (int i = 0; i < aeiou.length; i++) {
+                for (int j = 0; j < input.length(); j++) {
+                    if (aeiou[i] == divide[j]) {
+                        count++;
+                    }
+                }
+            }
