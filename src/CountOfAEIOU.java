eadb9c139f9fa7934b5f2e5472969e39c6c4f105
Alex Kim <alexkjh2011@gmail.com>
2023-03-12 22:26:02 +0100

    put scanner and  char[]

--- src/CountOfAEIOU.java
+++ src/CountOfAEIOU.java
@@ -2,3 +2,6 @@ import java.util.Scanner;
 
 public class CountOfAEIOU {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] aeiou = {'a', 'e', 'i', 'o', 'u'};
+
