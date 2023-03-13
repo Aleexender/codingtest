b738a6e6bb30428f73e97d0acccb08f961890a52
Alex Kim <alexkjh2011@gmail.com>
2023-03-13 21:14:43 +0100

    make some instance variable

--- src/MakingEnagram.java
+++ src/MakingEnagram.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class MakingEnagram {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        String otherInput = scanner.next();
+        String[] split = input.split("");
+        String[] otherSplit = otherInput.split("");
+        int answer = 0;
