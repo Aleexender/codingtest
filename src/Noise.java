7eaf07b3c1d532c3093c6b0b2f693863431d3919
Alex Kim <alexkjh2011@gmail.com>
2023-03-15 00:59:50 +0100

    have BigInteger int variables and String

--- src/Noise.java
+++ src/Noise.java
@@ -3,3 +3,7 @@ import java.util.Scanner;
 
 public class Noise {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger number = new BigInteger(scanner.next());
+        String multiple = scanner.next();
+        BigInteger number2 = new BigInteger(scanner.next());
