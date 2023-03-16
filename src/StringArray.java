3b3a58540546036fc83cd6dba4be6fcc921386e6
Alex Kim <alexkjh2011@gmail.com>
2023-03-16 18:37:50 +0100

    put Scanner and instance variables

--- src/StringArray.java
+++ src/StringArray.java
@@ -2,3 +2,6 @@ import java.util.Scanner;
 
 public class StringArray {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int repeat = scanner.nextInt();
+
