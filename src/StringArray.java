08f3dee882766fe067105c093a251219c716234b
Alex Kim <alexkjh2011@gmail.com>
2023-03-16 18:39:12 +0100

    make for() and change to String and print it

--- src/StringArray.java
+++ src/StringArray.java
@@ -5,3 +5,15 @@ public class StringArray {
         Scanner scanner = new Scanner(System.in);
         int repeat = scanner.nextInt();
 
+
+        for (int i = 0; i < repeat; i++) {
+            String input = scanner.next();
+            int length = input.length(); // 복습
+            String qwe = String.valueOf(input.charAt(0));
+            String qwer = String.valueOf(input.charAt(length - 1));
+
+            System.out.println(qwe + qwer);
+
+        }
+    }
+}
