15c8b27bb93305bfe5f99d53ed6a6365bdc412cd
Alex Kim <alexkjh2011@gmail.com>
2023-03-18 23:59:17 +0100

    Make instances variables

--- src/RTwo.java
+++ src/RTwo.java
@@ -1,3 +1,8 @@
 import java.util.Scanner;
 
 public class RTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int r1 = scanner.nextInt();
+        int average = scanner.nextInt();
+
