6a0607661a0b566ad08971cf18a863836321de3a
Alex Kim <alexkjh2011@gmail.com>
2023-03-21 23:58:54 +0100

    make for() and if() to not apply 2 and add all of nums

--- src/OddNumber.java
+++ src/OddNumber.java
@@ -6,3 +6,12 @@ public class OddNumber {
         List<Integer> oddNums = new ArrayList<>();
         int sum = 0;
         int count = 0;
+        for (int i = 0; i < 7; i++) {
+            int number = scanner.nextInt();
+
+            if (number % 2 != 0) {
+                count++;
+                sum += number;
+                oddNums.add(number);
+            }
+        }
