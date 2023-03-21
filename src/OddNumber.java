983b106effd0f62d06df9ba21d557533df84fcb1
Alex Kim <alexkjh2011@gmail.com>
2023-03-21 23:58:19 +0100

    make instance variables

--- src/OddNumber.java
+++ src/OddNumber.java
@@ -2,3 +2,7 @@ import java.util.*;
 
 public class OddNumber {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> oddNums = new ArrayList<>();
+        int sum = 0;
+        int count = 0;
