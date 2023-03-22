0c9754bca27ae0fc5818f05c63f126bc9f790f92
Alex Kim <alexkjh2011@gmail.com>
2023-03-22 02:43:43 +0100

    make instance variables and array

--- src/NoStudyAbroad.java
+++ src/NoStudyAbroad.java
@@ -4,3 +4,10 @@ import java.util.Scanner;
 
 public class NoStudyAbroad {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        char[] divide = input.toCharArray();
+        List<char[]> qwe = new LinkedList<>();
+        qwe.add(divide);
+        char[] cambridge = {'C', 'A', 'M', 'B', 'R', 'I', 'D', 'G', 'E'};
+
