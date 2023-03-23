9fccda69b5ceff6a7b93a5dbe412a3feba63eb87
Alex Kim <alexkjh2011@gmail.com>
2023-03-23 20:06:08 +0100

    make instances variables

--- src/VacationHomeWork.java
+++ src/VacationHomeWork.java
@@ -2,3 +2,10 @@ import java.util.Scanner;
 
 public class VacationHomeWork {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+        int english = scanner.nextInt(); // 숙제 페이지
+        int math = scanner.nextInt();
+        int possibleEn = scanner.nextInt(); // 일일당 할수있는 페이지
+        int possibleMa = scanner.nextInt();
+
