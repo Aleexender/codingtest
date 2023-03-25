eacca5ca17e6d2039244972dacdbfd8f8ff66bc6
Alex Kim <alexkjh2011@gmail.com>
2023-03-25 16:44:28 +0100

    make a instance variables

--- src/PartTimePCBang.java
+++ src/PartTimePCBang.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class PartTimePCBang {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean[] chair = new boolean[101];
+        int people = scanner.nextInt();
+        int count = 0;
+
