5544e5b8aa73baaf7c64dd853a2af0a4ae98b581
Alex Kim <alexkjh2011@gmail.com>
2023-03-26 19:04:17 +0200

    make instance variables

--- src/Snow.java
+++ src/Snow.java
@@ -1,3 +1,10 @@
 import java.util.Arrays;
 
 public class Snow {
+    public int[] solution(int n, int[] snow) {
+        int[] answer = {};
+        int[] mountain = new int[n];
+
+
+            int one = mountain.length;
+
