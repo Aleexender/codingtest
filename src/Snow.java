c46ead5f45ace7c8251cb1c59d61c51d214b4045
Alex Kim <alexkjh2011@gmail.com>
2023-03-26 19:04:46 +0200

    put for() and inside put if double time to get a answer

--- src/Snow.java
+++ src/Snow.java
@@ -8,3 +8,18 @@ public class Snow {
 
             int one = mountain.length;
 
+            for (int j = 0; j < mountain.length; j++) {
+                int snowflake = 2 * (n - one) + 1; // 눈을 지탱할수 있는 무게
+
+                mountain[j] = snowflake;
+                one--;
+
+                if (mountain[j] < snow[j]) {
+                    int fallDown = snow[j] - mountain[j];
+                    snow[j] -= fallDown;
+
+                    if (j != snow.length - 1) { // 맨끝이냐
+                        snow[j + 1] += fallDown;
+                    }
+                }
+            }
