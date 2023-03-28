37ced10143e78a5b70d558fdb014f968bb0108c7
Alex Kim <alexkjh2011@gmail.com>
2023-03-28 23:58:50 +0200

    make Scanner and instance variables

--- src/ThreeKangaroo.java
+++ src/ThreeKangaroo.java
@@ -1,3 +1,11 @@
 import java.util.Scanner;
 
 public class ThreeKangaroo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int max = b - a > c - b ? b - a : c - b;
