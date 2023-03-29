45de09b809c57853e3ca375f1710b90be452d6fe
Alex Kim <alexkjh2011@gmail.com>
2023-03-29 23:24:18 +0200

    make instance variables

--- src/lecture/FindWord.java
+++ src/lecture/FindWord.java
@@ -4,3 +4,15 @@ import java.util.Scanner;
 
 public class FindWord {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next().toLowerCase();
+        char input = scanner.next().charAt(0);
+        input = Character.toLowerCase(input);
+        int count = 0;
+
+
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == input) {
+//                count++;
+//            }
+//        }
