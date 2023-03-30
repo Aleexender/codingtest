03c017334dbcf263238401e252c96c1925110e03
Alex Kim <alexkjh2011@gmail.com>
2023-03-30 23:31:02 +0200

    put instance variables

--- src/EasyToSolve.java
+++ src/EasyToSolve.java
@@ -3,3 +3,10 @@ import java.util.Scanner;
 
 public class EasyToSolve {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        int first = scanner.nextInt();
+        int sec = scanner.nextInt();
+        int answer = 0;
+
