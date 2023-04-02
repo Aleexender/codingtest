ca04df90e85ed32754decc0c34f85740393d720f
Alex Kim <alexkjh2011@gmail.com>
2023-04-02 06:25:12 +0200

    Make instance variables

--- src/ConstantPart.java
+++ src/ConstantPart.java
@@ -2,3 +2,11 @@ import java.util.Scanner;
 
 public class ConstantPart {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            int[] arr = new int[8];
+            int[] count = new int[8];
+            boolean noRepeat = true;
+            String[] a = scanner.next().split("");
+            int max = 0;
+
