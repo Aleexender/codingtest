c9c99d173a8d341a8e0af6ad870894e4b4d95e5b
Alex Kim <alexkjh2011@gmail.com>
2023-04-08 01:54:35 +0200

    make instance variables

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class BaseballGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int players = scanner.nextInt();
+        int[] num = new int[28];
+        boolean[] valid = new boolean[28];
+        boolean onlyOne = false;
+            /*
