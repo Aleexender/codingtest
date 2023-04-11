f052589aa98bc4fc4a273950c0ef6df303dd134a
Alex Kim <alexkjh2011@gmail.com>
2023-04-11 13:10:24 +0200

    make instance variables

--- src/CalculateScore.java
+++ src/CalculateScore.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class CalculateScore {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int sum = 0;
+        int count2 = 0;
+        int[] num = new int[count];
+
