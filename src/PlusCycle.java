8993ea4dc23d7319cf37336f35e1851462b0519c
Alex Kim <alexkjh2011@gmail.com>
2023-04-14 16:00:20 +0200

    make instance variables

--- src/PlusCycle.java
+++ src/PlusCycle.java
@@ -1,3 +1,9 @@
 import java.util.Scanner;
 
 public class PlusCycle {
+
+    Scanner scanner = new Scanner(System.in);
+    int input = scanner.nextInt();
+    int count = 0;
+    int nextResult = input; // 비교해야하기 때문에 따로 만듬
+
