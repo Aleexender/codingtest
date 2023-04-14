0f688d2aa09b91a68531dae99b115f670d1889df
Alex Kim <alexkjh2011@gmail.com>
2023-04-14 16:00:53 +0200

    make logic method and put while and get number right and left

--- src/PlusCycle.java
+++ src/PlusCycle.java
@@ -7,3 +7,20 @@ public class PlusCycle {
     int count = 0;
     int nextResult = input; // 비교해야하기 때문에 따로 만듬
 
+
+
+
+    public void logic() {
+
+        while (true) {
+            int left = nextResult / 10;
+            int right = nextResult % 10;
+            int currentResult = left + right;
+            nextResult = right * 10 + currentResult % 10;
+            count++;
+            if (input == nextResult) {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
