d47645b700aedb2b95dde6b19f90482db263553c
Alex Kim <alexkjh2011@gmail.com>
2023-04-17 19:53:39 +0200

    make instance variables

--- src/MostBigNum.java
+++ src/MostBigNum.java
@@ -1,3 +1,9 @@
 import java.util.Scanner;
 
 public class MostBigNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] num = new int[9][9];
+        int max = 0;
+        int colum = 0;
+        int row = 0;
