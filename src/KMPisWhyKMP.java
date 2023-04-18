adaf4f1a2f3cbb789407adc78fc3961cf9e1af6b
Alex Kim <alexkjh2011@gmail.com>
2023-04-18 20:26:09 +0200

    put for to get answer

--- src/KMPisWhyKMP.java
+++ src/KMPisWhyKMP.java
@@ -5,3 +5,9 @@ public class KMPisWhyKMP {
         Scanner scanner = new Scanner(System.in);
         String word = scanner.nextLine();
         String[] divide =word.split("-");
+
+        for (int i = 0; i < divide.length; i++) {
+            System.out.print(divide[i].charAt(0));
+        }
+    }
+}
