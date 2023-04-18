17bcdb9309af4a11a2b92bec517b94abb90827a5
Alex Kim <alexkjh2011@gmail.com>
2023-04-18 20:26:00 +0200

    make instance variables

--- src/KMPisWhyKMP.java
+++ src/KMPisWhyKMP.java
@@ -2,3 +2,6 @@ import java.util.Scanner;
 
 public class KMPisWhyKMP {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String[] divide =word.split("-");
