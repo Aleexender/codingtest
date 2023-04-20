de8e3b13803a0bfe5960b1a7f5f05155473183cb
Alex Kim <alexkjh2011@gmail.com>
2023-04-21 01:56:02 +0200

    put instance variables

--- src/CountAlpha.java
+++ src/CountAlpha.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class CountAlpha {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        char[] split = input.toCharArray();
+        int[] alpha = new int[26];
+
