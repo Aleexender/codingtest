71b104161a2da50e1b1ee5ed822d5004882b74eb
Alex Kim <alexkjh2011@gmail.com>
2023-04-20 11:03:25 +0200

    information

--- src/ConferenceOrIntern.java
+++ src/ConferenceOrIntern.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class ConferenceOrIntern {
     public static void main(String[] args) {
+        /* 여자 2명 남자 1명
+        1. 여자랑 남자중 높은 숫자에서 - 인턴을 뺸다.
+        2. 여자는 2씩 뺸다
+        3. 남자는 1씩 빠진다
+        4. 남자 여자 둘다 되면 카운트 +1 ( 그룹결성 완료)
+         */
