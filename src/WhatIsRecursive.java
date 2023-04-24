f50de9a673e3618847fb1606ead33e3500965e6c
Alex Kim <alexkjh2011@gmail.com>
2023-04-24 02:48:01 +0200

    Instance Variables

--- src/WhatIsRecursive.java
+++ src/WhatIsRecursive.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class WhatIsRecursive {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+        recursive(input);
+
