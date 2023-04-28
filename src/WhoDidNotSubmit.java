d044bcd029cf53aa0587f66292797fc4ab6cb2c2
Alex Kim <alexkjh2011@gmail.com>
2023-04-28 22:54:46 +0200

    make instances

--- src/WhoDidNotSubmit.java
+++ src/WhoDidNotSubmit.java
@@ -4,3 +4,8 @@ import java.io.InputStreamReader;
 
 public class WhoDidNotSubmit {
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int studentNumWhoCall = 28;
+        final int totalStudent = 30;
+        boolean arr[] = new boolean[31];
+
