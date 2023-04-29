12ea3de21e8a13daa6a66fa40ec4b7a96787b464
Alex Kim <alexkjh2011@gmail.com>
2023-04-29 19:49:17 +0200

    make instance variables

--- src/SmartTrain.java
+++ src/SmartTrain.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 
 public class SmartTrain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] qwe = new int[4];
+        int most = 0;
+        int now = 0;
+
