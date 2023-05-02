dc465e84c0f07ca0fb920bc9c08cac9dd9956fb4
Alex Kim <alexkjh2011@gmail.com>
2023-05-02 02:19:45 +0200

    put instance variables

--- src/TestScore2.java
+++ src/TestScore2.java
@@ -2,3 +2,7 @@ import java.util.Scanner;
 
 public class TestScore2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int SUBJECT = 8;
+        int result1 = 0;
+        int result2 = 0;
