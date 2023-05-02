920a9f5aeb507d127168314585ec6ecd5f29e23f
Alex Kim <alexkjh2011@gmail.com>
2023-05-02 02:20:05 +0200

    put for() and put if() else

--- src/TestScore2.java
+++ src/TestScore2.java
@@ -6,3 +6,19 @@ public class TestScore2 {
         final int SUBJECT = 8;
         int result1 = 0;
         int result2 = 0;
+        for (int i = 0; i < SUBJECT; i++) {
+            int score = scanner.nextInt();
+            if (i >= 4) {
+                result1 += score;
+            } else {
+                result2 += score;
+            }
+        }
+
+        if (result1 > result2) {
+            System.out.println(result1);
+        } else {
+            System.out.println(result2);
+        }
+    }
+}
