40ecd211eb46108ff41f6ede5459c378a949bdae
Alex Kim <alexkjh2011@gmail.com>
2023-05-03 13:00:35 +0200

    make instance variables

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        long[] dp = new long[n + 1];
+
