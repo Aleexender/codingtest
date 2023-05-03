baa3b3cd2117298748cf548f5da840fa89bb253b
Alex Kim <alexkjh2011@gmail.com>
2023-05-03 13:00:52 +0200

    make logic of fibonacci and print it

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -7,3 +7,14 @@ public class Fibonacci {
 
         long[] dp = new long[n + 1];
 
+        dp[0] = 0;
+        if (n > 0) {
+            dp[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
