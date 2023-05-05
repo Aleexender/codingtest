7b9141ab8cfae0e5b3f615133448fcc2ba35ef9b
Alex Kim <alexkjh2011@gmail.com>
2023-05-05 16:06:08 +0200

    put if() and print it

--- src/SpecialDay.java
+++ src/SpecialDay.java
@@ -6,3 +6,23 @@ public class SpecialDay {
         int month = scanner.nextInt();
         int day = scanner.nextInt();
 
+        if (month > 2) {
+            System.out.println("After");
+        }
+
+        if (month < 2) {
+            System.out.println("Before");
+        }
+
+        if (month == 2) {
+            if (day == 18) {
+                System.out.println("Special");
+            }
+            if (day > 18) {
+                System.out.println("After");
+            } else {
+                System.out.println("Before");
+            }
+        }
+    }
+}
