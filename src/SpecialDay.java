c3eeded34ddecc89430d0657ca91141dc60e5608
Alex Kim <alexkjh2011@gmail.com>
2023-05-05 16:05:55 +0200

    build instance variable

--- src/SpecialDay.java
+++ src/SpecialDay.java
@@ -2,3 +2,7 @@ import java.util.Scanner;
 
 public class SpecialDay {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+
