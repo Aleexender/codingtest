44dca852f3160ecbb7d929a57e7e38736ab31042
Alex Kim <alexkjh2011@gmail.com>
2023-05-08 14:36:47 +0200

    make instance variable

--- src/BiggestSmaller.java
+++ src/BiggestSmaller.java
@@ -3,3 +3,7 @@ import java.util.Scanner;
 
 public class BiggestSmaller {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numQuan = scanner.nextInt();
+        int[] nums = new int[numQuan];
+
