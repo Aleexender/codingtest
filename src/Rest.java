4ad9f4946f63cd63fd06d03e004378a060807677
Alex Kim <alexkjh2011@gmail.com>
2023-05-11 20:08:03 +0200

    make stream()

--- src/Rest.java
+++ src/Rest.java
@@ -10,3 +10,7 @@ public class Rest {
         for (int i = 0; i < 10; i++) {
             nums.add(scanner.nextInt());
         }
+
+        nums.stream().map(a -> a % 42).forEach(b -> answer.add(b));
+        Long count = nums.stream().map(b -> b % 42).distinct().count();
+
