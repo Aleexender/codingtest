45333e4bdf1ace9f8ed9a78208d76b9591c6da52
Alex Kim <alexkjh2011@gmail.com>
2023-05-11 20:07:37 +0200

    make instance variables

--- src/Rest.java
+++ src/Rest.java
@@ -3,3 +3,7 @@ import java.util.stream.Collectors;
 
 public class Rest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> nums = new ArrayList<>();
+        HashSet<Integer> answer = new HashSet<>();
+
