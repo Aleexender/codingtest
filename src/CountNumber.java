6e7d72ac582c79955082114dc1494c648085f1f3
Alex Kim <alexkjh2011@gmail.com>
2023-05-13 20:45:02 +0200

    put instance variables

--- src/CountNumber.java
+++ src/CountNumber.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 
 public class CountNumber {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[] numbers = new int[num];
+        int result = 0;
+
