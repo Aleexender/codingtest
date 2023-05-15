1e4993a1da7301e3897d6f49d3c247f4de594fb3
Alex Kim <alexkjh2011@gmail.com>
2023-05-15 21:48:05 +0200

    make instance variables

--- src/LessThanX.java
+++ src/LessThanX.java
@@ -2,3 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class LessThanX {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberQuan = scanner.nextInt();
+        int numLimit = scanner.nextInt();
+        int[] nums = new int[numberQuan];
