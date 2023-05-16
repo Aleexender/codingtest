929022c7ad097ab664946da5e812bfb7c4b5f507
Alex Kim <alexkjh2011@gmail.com>
2023-05-16 12:59:21 +0200

    put instance variables

--- src/TheMostBigGemMinSu.java
+++ src/TheMostBigGemMinSu.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 
 public class TheMostBigGemMinSu {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
