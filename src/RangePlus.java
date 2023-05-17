eebee020ca785ccc15028a30f3a978f2054608c9
Alex Kim <alexkjh2011@gmail.com>
2023-05-17 23:21:34 +0200

    make instance variable

--- src/RangePlus.java
+++ src/RangePlus.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 
 public class RangePlus {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row = scanner.nextInt();
+        int vertical = scanner.nextInt();
+
+        int[][] arrArr = new int[row][vertical];
