5a46d67b7459845ac0667e5a172107a348104954
Alex Kim <alexkjh2011@gmail.com>
2023-05-17 23:21:44 +0200

    make for()

--- src/RangePlus.java
+++ src/RangePlus.java
@@ -8,3 +8,11 @@ public class RangePlus {
         int vertical = scanner.nextInt();
 
         int[][] arrArr = new int[row][vertical];
+
+        for (int i = 0; i < 2; i++) { //2번 반복 된다
+            for (int z = 0; z < row; z++) { //
+                for (int j = 0; j < vertical; j++) { //0,1 0,2 0,3  || 1,1 1,2 1,3  || 2,1 2,2 2,3
+                    arrArr[z][j] += scanner.nextInt();
+                }
+            }
+        }
