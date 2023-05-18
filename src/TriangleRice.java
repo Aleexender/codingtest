da085b48a72924dbff5a61a1f52485a5d7105831
Alex Kim <alexkjh2011@gmail.com>
2023-05-18 23:15:08 +0200

    put instance variables

--- src/TriangleRice.java
+++ src/TriangleRice.java
@@ -2,3 +2,9 @@ import java.util.Scanner;
 
 public class TriangleRice {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int gram = scanner.nextInt();
+        int price = scanner.nextInt();
+        int count = scanner.nextInt();
+        double[] totalPrice = new double[count+1];
+        double minPrice = 0;
