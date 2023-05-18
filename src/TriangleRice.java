cb52abf32db854050b6cded3aaa2dc58096d2019
Alex Kim <alexkjh2011@gmail.com>
2023-05-18 23:15:20 +0200

    put two force

--- src/TriangleRice.java
+++ src/TriangleRice.java
@@ -8,3 +8,11 @@ public class TriangleRice {
         int count = scanner.nextInt();
         double[] totalPrice = new double[count+1];
         double minPrice = 0;
+        for (int i = 1; i < count+1; i++) {
+            int otherGram = scanner.nextInt();
+            int otherPrice = scanner.nextInt();
+
+
+            totalPrice[0] = (double) gram / price * 1000;
+            totalPrice[i] = (double) otherGram / otherPrice * 1000;
+        }
