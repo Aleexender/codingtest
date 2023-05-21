f8fb9d40ed67e9df277e54e493ddd61dcafcb656
Alex Kim <alexkjh2011@gmail.com>
2023-05-21 13:41:31 +0200

    make a instance variables

--- src/Constant.java
+++ src/Constant.java
@@ -3,3 +3,11 @@ import java.util.Stack;
 
 public class Constant {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.next();
+        String num2 = scanner.next();
+        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+        String save = "";
+        String save2 = "";
+
