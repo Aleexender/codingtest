26fac9a475240e0ab148d039232b8e62d3a407c8
Alex Kim <alexkjh2011@gmail.com>
2023-05-21 13:41:57 +0200

    make for() and put final

--- src/Constant.java
+++ src/Constant.java
@@ -11,3 +11,28 @@ public class Constant {
         String save = "";
         String save2 = "";
 
+
+        final int threeNum = 3;
+
+        for (int i = 0; i < threeNum; i++) {
+            stack.push(num.charAt(i));
+        }
+        for (int k = 0; k < threeNum; k++) {
+            stack2.push(num2.charAt(k));
+        }
+
+        for (int j = 0; j < threeNum; j++) {
+           save += stack.pop();
+            save2 +=stack2.pop();
+        }
+        if (save.compareTo(save2) > 0) {
+            System.out.println(save);
+        } else {
+            System.out.println(save2);
+        }
+
+//        if (Integer.parseInt(save) < Integer.parseInt(save2)) {
+//
+//        }
+    }
+}
