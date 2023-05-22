9d2143dc55a3531e2d621d338856ba90b022b847
Alex Kim <alexkjh2011@gmail.com>
2023-05-22 23:21:45 +0200

    put for() and print it

--- src/Stackcode.java
+++ src/Stackcode.java
@@ -8,3 +8,33 @@ public class Stackcode {
         Stack<String> stack = new Stack<>();
         StringBuilder stringBuilder = new StringBuilder();
 
+        for (int i = 0; i < repeat; i++) {
+            String input = scanner.next(); //띄어쓰기 기준
+            if (input.equals("push")) {
+                stack.push(scanner.next());
+            } else if (input.equals("pop"))  {
+                if (stack.isEmpty()) {
+                   stringBuilder.append("-1"+"\n");
+                } else {
+                    stringBuilder.append(stack.pop()+"\n");
+                }
+            } else if (input.equals("size")) {
+                stringBuilder.append(stack.size()+"\n");
+            } else if (input.equals("empty")) {
+                if (stack.isEmpty()) {
+                    stringBuilder.append("1"+"\n");
+                } else {
+                    stringBuilder.append("0"+"\n");
+                }
+            } else {
+                if (!stack.isEmpty()) {
+                    stringBuilder.append(stack.peek()+"\n");
+                } else {
+                    stringBuilder.append("-1"+"\n");
+                }
+            }
+        }
+        System.out.println(stringBuilder.toString());
+
+    }
+}
