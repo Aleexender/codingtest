a0cbeb7871142b7da10ffb8ac2f6d10e67b118e8
Alex Kim <alexkjh2011@gmail.com>
2023-05-22 23:21:32 +0200

    make instance variables

--- src/Stackcode.java
+++ src/Stackcode.java
@@ -3,3 +3,8 @@ import java.util.Stack;
 
 public class Stackcode {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int repeat = scanner.nextInt();
+        Stack<String> stack = new Stack<>();
+        StringBuilder stringBuilder = new StringBuilder();
+
