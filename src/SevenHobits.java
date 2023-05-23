64b7d347e5f51175684e2e0a20c012634575c0c8
Alex Kim <alexkjh2011@gmail.com>
2023-05-24 00:47:21 +0200

    make instance variables

--- src/SevenHobits.java
+++ src/SevenHobits.java
@@ -2,3 +2,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SevenHobits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int FAKE_PEOPLE = 9;
+        int[] people = new int[FAKE_PEOPLE];
+        int sum = 0;
+
