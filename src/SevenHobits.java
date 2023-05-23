9ae9ad693198cd28c29774b968af619ccc9f5025
Alex Kim <alexkjh2011@gmail.com>
2023-05-24 00:47:43 +0200

    make for() and return it

--- src/SevenHobits.java
+++ src/SevenHobits.java
@@ -13,3 +13,22 @@ public class SevenHobits {
             sum += people[i];
         }
 
+        for (int i = 0; i < FAKE_PEOPLE -1; i++) {
+            for (int j = i+1; j < FAKE_PEOPLE; j++) {
+                if (sum - people[i] - people[j] == 100) {
+                    people[i] = 0;
+                    people[j] = 0;
+                    Arrays.sort(people);
+                    for (int k = 0; k < FAKE_PEOPLE; k++) {
+                        if (people[k] == 0) {
+
+                        } else {
+                            System.out.println(people[k]);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
