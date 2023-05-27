6739484ab3d8709e1b3d4f614319d0447023a2ea
Alex Kim <alexkjh2011@gmail.com>
2023-05-27 13:50:43 +0200

    edit one

---
+++ src/StrangeMultiply.java
@@ -0,0 +1,22 @@
+//import java.util.Scanner;
+//
+//public class StrangeMultiply {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String[] num = scanner.next().split("");
+//        String[] num1 = scanner.next().split("");
+//        long result = 0;
+//
+//        for (int i = 0; i < num.length; i++) {
+//            for (int j = 0; j < num1.length; j++) {
+//                int number = Integer.parseInt(num[i]);
+//                int number1 = Integer.parseInt(num1[j]);
+//                result += number * number1;
+//
+//            }
+//        }
+//        System.out.println(result);
+//    }
+//}
+//
+
