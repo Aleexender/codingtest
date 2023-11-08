24c6f092b3b497e31bf0ab889fd59f7db7fb690a
Alex Kim <alexkjh2011@gmail.com>
2023-11-09 03:53:35 +0900

    Create QuadTree.java

---
+++ src/QuadTree.java
@@ -0,0 +1,48 @@
+//import java.util.Scanner;
+//
+//public class QuadTree {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int count = scanner.nextInt();
+//        int[][] arr = new int[count][count];
+//        for (int i = 0; i < count; i++) {
+//                String input = scanner.next();
+//        }
+//    }
+//
+//    static int function(int blackOrWhite , int[][]arr) {
+//        //base case  all 흑, all 백
+//        if (is all zero){  // 배열 하나하나씩 확인을 해서 0 인지 1인지 확인, 모든 수를 검사를 하는것
+//            return 0;
+//        } else{
+//            return 1;
+//        }
+//        for (int i = 0; i < 4; i++) {
+//        }
+//
+//    }
+//    static  boolean isAllZero(int[][] arr) {
+//
+//
+//        for (int i = 0; i < arr.length; i++) {
+//
+//
+//            for (int x = 0; x < arr[0].length; x++) {
+//                if (arr[i][x] == 1) {
+//                    return false;
+//                }
+//
+//            }
+//        }
+//    }
+//
+//
+//
+//
+//
+//        // recursive case
+//        // 2차배열을 8*8 = 64 -> 4*4 = 16;
+//         function()
+//
+//    }
+//}
