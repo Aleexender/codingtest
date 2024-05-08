23e7a691910ed69d9523399cee4830bfbaa45538
Aleexender <alexkjh2011@gmail.com>
2024-05-08 14:45:08 +0900

    Create Onboarding.java

---
+++ src/woowacourse/Onboarding.java
@@ -0,0 +1,17 @@
+package woowacourse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Onboarding {
+
+    public void solution() {
+        int pobi = 0;
+        int crong = 0;
+        List list = new ArrayList(2);
+        list.size();
+
+
+
+    }
+}
